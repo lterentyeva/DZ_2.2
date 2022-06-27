a71b29a47a7274540bc5b5f02999eca1cf378a9f
lterentyeva <lterentyeva@yandex.ru>
2022-06-27 03:09:10 +0300

    Add test

--- src/test/java/ru/netology/CardDelivery.java
+++ src/test/java/ru/netology/CardDelivery.java
@@ -3,13 +3,37 @@ package ru.netology;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.open;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
 
 public class CardDelivery {
 
+
+    public String nextDate(int days) {
+        LocalDate parsedDate = LocalDate.now();
+        LocalDate addedDate = parsedDate.plusDays(days);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        return addedDate.format(formatter);
+    }
+
+
     @Test
     void shouldSuccessfulSending() {
+        String mittingData = nextDate(3);
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
+        $("input[placeholder=\"Город\"]").val("Санкт-Петербург");
+        $("input[placeholder=\"Дата встречи\"]").doubleClick().sendKeys(mittingData);
+        $("input[name=\"name\"]").val("Иванов Иван");
+        $("input[name=\"phone\"]").val("+79111234567");
+        $("[data-test-id=agreement] span").click();
+        $x("//span[text()=\"Забронировать\"]").click();
+        $x("//*[contains(text(),\"Встреча успешно забронирована на \")]").should(visible, Duration.ofSeconds(15));
     }
 }
